ad1f28c79ad9e2d0bf4cf3d94693589e09b96a85
ramyamanda <mandaramyachowdary@gmail.com>
2023-03-21 20:49:15 +0530

    added crud operatrions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { HomeComponent } from './home/home.component';
+import { NotAvailableComponent } from './not-available/not-available.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'home', pathMatch:'full' },
+  { path: 'add', component: AddComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'home/:id', component: UserDetailsComponent },
+  { path: 'home/:id/edit',component:EditComponent },
+  { path:'**', component:NotAvailableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routings = [
+  AddComponent,
+  HomeComponent,
+  EditComponent,
+  UserDetailsComponent,
+];
